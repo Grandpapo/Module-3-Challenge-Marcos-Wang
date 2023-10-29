09429238bb738e3cbcd72ec648a8569c3d385d0c
Marcos Wang <67712814+Marcos-Wang@users.noreply.github.com>
2023-10-28 17:11:21 -0700

    Updated script.js to add all required functionalities.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,34 @@
 // Assignment code here
+function generatePassword(){
+  let myPassword = "";
+  let validchars = "";
+  let letter;
+  let arrayLength = prompt('choose a password length from 8 to 128 characters');
+  if (confirm('include lowercase?')){
+    validchars += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if(confirm('include uppercase?')){
+    validchars += "ABCDEFGHIJKLMONPQRSTUVXYZ";
+  }
+  if(confirm('include numbers?')){
+    validchars += "0123456789";
+  }
+  if(confirm('include special characters?')){
+    validchars += "!@#$%&";
+  }
 
+
+
+  for (let i =  1; i<=parseFloat(arrayLength); i++){
+
+    letter = validchars[Math.floor((Math.random() * validchars.length)+1)]
+    myPassword+= letter;
+
+  }
+
+  return myPassword;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,7 +37,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
